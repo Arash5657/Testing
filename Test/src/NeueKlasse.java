b2154dd05df4f34df5ea98a24b1889433eeaf5bf
Arash <Arash@Arash-PC.localdomain>
2016-08-10 19:52:42 +0200

    mm

--- Test/src/NeueKlasse.java
+++ Test/src/NeueKlasse.java
@@ -5,6 +5,5 @@ public class NeueKlasse {
 	public static void main(String[] args){
 		System.out.println("Neueklasse1");
 		System.out.println("Neueklasse2");
-		System.out.println("Neueklasse3");
 	}
 }
