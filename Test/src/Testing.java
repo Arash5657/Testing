f7d3fbac3e0b47a753e2a53f719a86958cf30bf6
Arash <Arash@Arash-PC.localdomain>
2016-08-10 19:46:13 +0200

    Aenderung in Testing

--- Test/src/Testing.java
+++ Test/src/Testing.java
@@ -2,8 +2,8 @@
 public class Testing {
 
 	public static void main(String[] args) {
-		System.out.println("Helloooo");
-		System.out.println("iooioioioi");
-		System.out.println("ooooooooooopppppppppppppp");
+		System.out.println("jafiiiii");
+		System.out.println("jafuuuuuuuuuu");
+		System.out.println("jafff");
 	}
 }
