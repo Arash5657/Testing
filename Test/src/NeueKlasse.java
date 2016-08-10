46dd881d1aca0c52e0248473b7f689224cbf4efc
Arash <Arash@Arash-PC.localdomain>
2016-08-10 19:49:35 +0200

    neue Aenderung

--- Test/src/NeueKlasse.java
+++ Test/src/NeueKlasse.java
@@ -3,6 +3,7 @@ public class NeueKlasse {
     
 	
 	public static void main(String[] args){
-		System.out.println("Neueklasse");
+		System.out.println("Neueklasse1");
+		System.out.println("Neueklasse2");
 	}
 }
