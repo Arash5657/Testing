3daa3c862eadd66a4fd067c7f1b51ff3a343f8a7
Arash5657 <arash_hsn2002@yahoo.com>
2016-08-10 21:31:21 +0200

    Update NeueKlasse.java
    update

--- Test/src/NeueKlasse.java
+++ Test/src/NeueKlasse.java
@@ -5,5 +5,7 @@ public class NeueKlasse {
 	public static void main(String[] args){
 		System.out.println("Neueklasse1");
 		System.out.println("Neueklasse2");
+	        System.out.println("Neueklasse27877");
+		
 	}
 }
